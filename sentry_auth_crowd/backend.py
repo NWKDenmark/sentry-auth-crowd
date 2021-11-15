c499c3aad6a3531885e09b2a8e5ecf0a70bd7bc1
NWK <nwkdenmark@protonmail.com>
2021-11-15 09:34:12 +0100

    Updated plugin for python3 and Sentry 21.9

--- sentry_auth_crowd/backend.py
+++ sentry_auth_crowd/backend.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
 import logging
 
 from django.contrib.auth.backends import ModelBackend
@@ -13,8 +11,6 @@ from sentry.models import AuditLogEntry, AuditLogEntryEvent
 import crowd
 
 from .constants import CROWD_URL, CROWD_APP_NAME, CROWD_APP_PASSWORD
-from .constants import CROWD_DEFAULT_TEAM_SLUGS
-
 
 def new_user(username, success):
     # see https://github.com/getsentry/sentry/blob/master/src/sentry/auth/helper.py
@@ -27,75 +23,51 @@ def new_user(username, success):
             name=name,
             is_managed=True,
         )
-        if not UserEmail.objects.filter(user=user).exists():
-            user_email = UserEmail.objects.create(user=user)
-            user_email.email = email
-            user_email.save()
 
-        if not OrganizationMember.objects.filter(user=user).exists():
-            organization = Organization.get_default()
+        organization = Organization.get_default()
+        om = OrganizationMember.objects.create(
+            organization=organization,
+            role=organization.default_role,
+            user=user,
+            flags=OrganizationMember.flags['sso:linked'],
+        )
 
-            om = OrganizationMember.objects.create(
-                organization=organization,
-                role=organization.default_role,
-                user=user,
-                flags=getattr(OrganizationMember.flags, 'sso:linked'),
-            )
-
-            if CROWD_DEFAULT_TEAM_SLUGS:
-                for team_slug in set(CROWD_DEFAULT_TEAM_SLUGS):
-                    try:
-                        team = Team.objects.get(slug=team_slug)
-                    except Team.DoesNotExist:
-                        logging.exception("Unable to fetch Team with slug '%s'",
-                                          team_slug)
-                    else:
-                        OrganizationMemberTeam.objects.create(
-                            team=team,
-                            organizationmember=om,
-                        )
-
-            AuditLogEntry.objects.create(
-                organization=organization,
-                actor=user,
-                target_object=om.id,
-                target_user=om.user,
-                event=AuditLogEntryEvent.MEMBER_ADD,
-                data=om.get_audit_log_data(),
-            )
+        AuditLogEntry.objects.create(
+            organization=organization,
+            actor=user,
+            # TODO attribute request ip
+            #ip=
+            target_object=om.id,
+            target_user=om.user,
+            event=AuditLogEntryEvent.MEMBER_ADD,
+            data=om.get_audit_log_data(),
+        )
         return user
 
 
 class SentryCrowdBackend(ModelBackend):
     """
-    Authenticate against Crowd server.
+    Mimick email backend and authenticate against Crowd server.
 
-    Auto create account if not exists already
+    Auto create account if not exists already.
     """
-
+    # According to https://github.com/getsentry/sentry/blob/21.9.0/src/sentry/conf/server.py
+    # Only email backend is supported by "default"
+    # See https://github.com/getsentry/sentry/blob/21.9.0/src/sentry/utils/auth.py 
+    # for structure of Email backend.
     def __init__(self, *args, **kwargs):
         self._crowd = crowd.CrowdServer(CROWD_URL, CROWD_APP_NAME, CROWD_APP_PASSWORD)
         super(SentryCrowdBackend, self).__init__(*args, **kwargs)
 
-    def authenticate(self, username=None, password=None):
-        if username is None or password is None:
-            return None
-
+    def authenticate(self, request, username=None, password=None):
         success = self._crowd.auth_user(username, password)
         if success:
             try:
                 user = User.objects.filter(is_active=True).exclude(password='!').get(username=username)
             except User.DoesNotExist:
                 user = new_user(username, success)
-
             return user
-
-        # Example of how token based (SSO) authentication looks like
-        #
-        # # Create a session. The important bit is the token.
-        # session = cs.get_session(username, password)
-        #
-        # # Check that the token is valid (and of course it should be).
-        # success = cs.validate_session(session['token'])
-
         return None
+
+    def user_can_authenticate(self, user):
+        return True
--- sentry_auth_crowd/constants.py
+++ sentry_auth_crowd/constants.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function
-
 from django.conf import settings
-
 
 CROWD_URL = getattr(settings, 'CROWD_URL', None)
 CROWD_APP_NAME = getattr(settings, 'CROWD_APP_NAME', None)
 CROWD_APP_PASSWORD = getattr(settings, 'CROWD_APP_PASSWORD', None)
-CROWD_DEFAULT_TEAM_SLUGS = getattr(settings, 'CROWD_DEFAULT_TEAM_SLUGS', None)
